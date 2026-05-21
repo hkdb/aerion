8f3850ed2712ebffe52fa43c1058718bd78c0011
Gerti1972 <gerti@gerti.de>
2026-05-21 10:00:24 +0200

    Add German locale support for date-fns

--- frontend/src/lib/i18n/dateFnsLocale.ts
+++ frontend/src/lib/i18n/dateFnsLocale.ts
@@ -27,6 +27,11 @@ export async function loadDateFnsLocale(code: string): Promise<Locale | undefine
       dateFnsLocale = mod.cs
       break
     }
+    case 'de': {
+      const mod = await import('date-fns/locale/de')
+      dateFnsLocale = mod.de
+      break
+    }
     case 'fr': {
       const mod = await import('date-fns/locale/fr')
       dateFnsLocale = mod.fr
@@ -35,11 +40,6 @@ export async function loadDateFnsLocale(code: string): Promise<Locale | undefine
     case 'nb': {
       const mod = await import('date-fns/locale/nb')
       dateFnsLocale = mod.nb
-      break
-    }
-    case 'de': {
-      const mod = await import('date-fns/locale/de')
-      dateFnsLocale = mod.de
       break
     }
     case 'zh-CN': {
